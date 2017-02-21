cbc6be4ea1b1c5e2da3b570e704d75d6bbe54d97
olivier <olipic96@hotmail.com>
2017-02-21 17:43:59 -0500

    ex7

---
+++ ex7.js
@@ -0,0 +1,31 @@
+var http = require("http");//importe le module serveur http
+var fs = require('fs');
+var obj;
+fs.readFile('fichierJson.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  obj = JSON.parse(data);
+
+
+});
+
+
+var monObjet=obj;
+var ch="<table>"
+function affiche_objet(monObjet){
+	for (propriete in monObjet){
+		ch+="<tr>"+"<td>"+propriete+'</td>'+'<td>'+monObjet[propriete]+'</td>'+"</tr>"
+
+	}
+	ch+="</table>"
+	return ch;
+}
+
+
+
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<h1>Hello World</h1>");
+  response.write(affiche_objet(obj));
+  response.end();
+}).listen(3000);
